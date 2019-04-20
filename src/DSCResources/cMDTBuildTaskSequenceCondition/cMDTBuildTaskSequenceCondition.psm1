8f8cd96b4ead60a2bba63fada73c0e7374a9783d
Pavel Andreev <pvs043@outlook.com>
2019-04-20 22:02:35 +0500

    Add new DSCresource

---
+++ src/DSCResources/cMDTBuildTaskSequenceCondition/cMDTBuildTaskSequenceCondition.psm1
@@ -0,0 +1,64 @@
+enum Ensure
+{
+    Absent
+    Present
+}
+
+[DscResource()]
+class cMDTBuildTaskSequenceCondition
+{
+    # Task Sequence File
+    [DscProperty(Key)]
+    [string]$TSFile
+
+    # Step name
+    [DscProperty(Key)]
+    [string]$Name
+
+    # Group for step
+    [DscProperty(Mandatory)]
+    [string]$GroupName
+
+    [DscProperty(Mandatory)]
+    [string]$PSDriveName
+
+    [DscProperty(Mandatory)]
+    [string]$PSDrivePath
+
+    [void] Set()
+    {
+	}
+
+    [bool] Test()
+    {
+		$TS = $this.LoadTaskSequence()
+        $present = $false
+		$group = $TS.sequence.group | Where-Object {$_.Name -eq $this.GroupName}
+        if ($this.Type -eq "Group") {
+            $step = $group.group | Where-Object {$_.Name -eq $this.Name}
+        }
+        else {
+            $step = $group.step | Where-Object {$_.Name -eq $this.Name}
+        }
+		
+		$operator = $step.condition.operator
+		if ($operator) {
+			$type = $operator.type
+		}
+		
+		return $present
+	}
+
+    [cMDTBuildTaskSequenceCondition] Get()
+    {
+		return $this
+	}
+
+    [xml] LoadTaskSequence()
+    {
+        $tsPath = $this.TSFile
+        $xml = [xml](Get-Content $tsPath)
+        return $xml
+    }
+}
+
